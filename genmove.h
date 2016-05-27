b2e321c25c183be32f0f95b8661795f8f98a345f
Kuma <kuma@apple-no-MacBook-Air.local>
2016-05-27 15:11:27 +0900

    fix

--- genmove.h
+++ genmove.h
@@ -1,5 +1,27 @@
 #ifndef _GENMOVE_H_
 #define _GENMOVE_H_
 
+int GenPawnMove_b( struct Position*, int, Move* );
+int GenLanceMove_b( struct Position*, int, Move* );
+int GenKnightMove_b( struct Position*, int, Move* );
+int GenSilverMove_b( struct Position*, int, Move* );
+int GenGoldMove_b( struct Position*, int, Move* );
+int GenKingMove_b( struct Position*, int, Move* );
+int GenBishopMove_b( struct Position*, int, Move* );
+int GenRookMove_b( struct Position*, int, Move* );
+int GenHorseMove_b( struct Position*, int, Move* );
+int GenDragonMove_b( struct Position*, int, Move* );
+
+int GenPawnMove_w( struct Position*, int, Move* );
+int GenLanceMove_w( struct Position*, int, Move* );
+int GenKnightMove_w( struct Position*, int, Move* );
+int GenSilverMove_w( struct Position*, int, Move* );
+int GenGoldMove_w( struct Position*, int, Move* );
+int GenKingMove_w( struct Position*, int, Move* );
+int GenBishopMove_w( struct Position*, int, Move* );
+int GenRookMove_w( struct Position*, int, Move* );
+int GenHorseMove_w( struct Position*, int, Move* );
+int GenDragonMove_w( struct Position*, int, Move* );
+
 
 #endif //_GENMOVE_H_
